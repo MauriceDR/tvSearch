28152edafa00d9cec2a0908ec62dcc450c62746f
Maurice Roach <mroach1993@hotmail.com>
2022-09-28 18:48:03 -0400

    results now append to a div instead of body

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const form = document.querySelector("#searchForm");
+const results = document.querySelector(".results");
+const container = document.querySelector(".resultContainer");
+const btn = document.querySelector("button");
+
 form.addEventListener("submit", async function (e) {
   e.preventDefault();
   console.log("Submitted");
@@ -6,7 +10,21 @@ form.addEventListener("submit", async function (e) {
   const res = await axios.get(
     `http://api.tvmaze.com/search/shows?q=${searchTerm}`
   );
-  const img = document.createElement("IMG");
-  img.src = res.data[0].show.image.medium;
-  document.body.append(img);
+  makeImages(res.data);
 });
+
+btn.addEventListener("click", () => {
+  container.classList.add("show");
+});
+
+const makeImages = function (shows) {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("IMG");
+      img.src = result.show.image.medium;
+      results.append(img);
+    }
+  }
+};
+
+// append images, results to new div created
